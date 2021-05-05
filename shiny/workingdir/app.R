2656d82ad2814e5d6942335c06ce04bb9a6260b1
kanyerezi30 <kanyerezi30@gmail.com>
2021-05-05 17:38:52 +0300

    Modified to a greeting app

--- shiny/workingdir/app.R
+++ shiny/workingdir/app.R
@@ -1,27 +1,48 @@
 ## This is my introduction to developing using shiny app
+### Practice
 library(shiny) # load the shiny package
+#ui <- fluidPage(
+#  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+#  verbatimTextOutput("summary"),
+#  tableOutput("table")
+#) # Define the user interface, the HTML webpage that humans interact with
+
+#server <- function(input, output, session) {
+  # Create a reactive expression
+#  dataset <- reactive({
+#    get(input$dataset, "package:datasets")
+#  })
+  
+#  output$summary <- renderPrint({
+    # Use a reactive expression by calling it like a function
+#    summary(dataset())
+#  })
+  
+  
+#  output$table <- renderTable({
+#    dataset()
+#  })
+#} # Specifies the behaviour of our app by defining a server function. \
+# It's currently empty, so our app doesn't do anything,but we'll be back to revisit this shortly
 ui <- fluidPage(
-  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
-  verbatimTextOutput("summary"),
-  tableOutput("table")
-) # Define the user interface, the HTML webpage that humans interact with
+  textInput("name", "What's your name?"),
+  numericInput("age", "How old are you?", value = NA),
+  textOutput("greeting")
+)
 
 server <- function(input, output, session) {
-  # Create a reactive expression
-  dataset <- reactive({
-    get(input$dataset, "package:datasets")
+  output$greeting <- renderText({
+    paste0("Hello ", input$name)
   })
-  
-  output$summary <- renderPrint({
-    # Use a reactive expression by calling it like a function
-    summary(dataset())
-  })
-  
-  
-  output$table <- renderTable({
-    dataset()
-  })
-} # Specifies the behaviour of our app by defining a server function. \
-# It's currently empty, so our app doesn't do anything,but we'll be back to revisit this shortly
-
+}
+#tableOutput("mortgage")
+#output$greeting <- renderText({
+#  paste0("Hello ", input$name)
+#})
+#numericInput("age", "How old are you?", value = NA)
+#textInput("name", "What's your name?")
+#textOutput("greeting")
+#output$histogram <- renderPlot({
+#  hist(rnorm(1000))
+#}, res = 96)
 shinyApp(ui, server) # Construct and start a shiny application from UI and server
