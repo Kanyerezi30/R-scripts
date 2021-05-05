b92400825d04bc9b41edf449ed47c4afe7b4d14a
kanyerezi30 <kanyerezi30@gmail.com>
2021-05-05 09:44:57 +0300

    Created app.R

---
+++ shiny/workingdir/app.R
@@ -0,0 +1,11 @@
+## This is my introduction to developing using shiny app
+library(shiny) # load the shiny package
+ui <- fluidPage(
+  "Hello, world!"
+) # Define the user interface, the HTML webpage that humans interact with
+
+server <- function(input, output, session) {
+} # Specifies the behaviour of our app by defining a server function. \
+# It's currently empty, so our app doesn't do anything,but we'll be back to revisit this shortly
+
+shinyApp(ui, server) # Construct and start a shiny application from UI and server
