139a0084b923237ce32ff39d27d4890cf065ad01
kanyerezi30 <kanyerezi30@gmail.com>
2021-05-05 21:16:21 +0300

    Modified the product

--- shiny/workingdir/app.R
+++ shiny/workingdir/app.R
@@ -40,18 +40,45 @@ library(shiny) # load the shiny package
 #  paste0("Hello ", input$name)
 #})
 
+# ui <- fluidPage(
+  # sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
+  # sliderInput("y", label = "and y is", min = 1, max = 50, value = 5),
+  # "then x times y is",
+  # textOutput("product")
+# )
+
+# server <- function(input, output, session) {
+  # output$product <- renderText({ 
+    # input$x * input$y
+  # })
+# }
+
+
 ui <- fluidPage(
-  sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
-  sliderInput("y", label = "and y is", min = 1, max = 50, value = 5),
-  "then x times y is",
-  textOutput("product")
+  sliderInput("x", "If x is", min = 1, max = 50, value = 30),
+  sliderInput("y", "and y is", min = 1, max = 50, value = 5),
+  "then, (x * y) is",
+  textOutput("product"),
+  "and, (x * y) + 5 is",
+  textOutput("product_plus5"),
+  "and (x * y) + 10 is",
+  textOutput("product_plus10")
 )
 
 server <- function(input, output, session) {
-  output$product <- renderText({ 
+  # Create a product reactive expression
+  pdt_exp <- reactive({
     input$x * input$y
+  })
+  output$product <- renderText({ 
+    pdt_exp()
+  })
+  output$product_plus5 <- renderText({ 
+    pdt_exp() + 5
+  })
+  output$product_plus10 <- renderText({ 
+    pdt_exp() + 10
   })
 }
 
-shinyApp(ui, server)
 shinyApp(ui, server) # Construct and start a shiny application from UI and server
